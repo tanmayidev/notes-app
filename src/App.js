05bb6f26a1667ecab723ac1bd5590704ce0cf5d7
tanmayidev <tanmayi.d.99@gmail.com>
2023-05-07 18:05:54 +0530

    add CreateNote component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Notes from "./components/Notes";
+import CreateNote from "./components/CreateNote";
 
 function App() {
   return (
     <div className="main">
       <Header />
       <Notes />
+      <CreateNote />
     </div>
   );
 }
